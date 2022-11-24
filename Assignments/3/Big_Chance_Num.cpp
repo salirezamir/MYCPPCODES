5e9c7ddcd563fb43feb14d6b713567c6cb473ec3
SAlirezaMir <mirabedini.alireza@gmail.com>
2022-11-25 02:23:09 +0330

    lucky num fix assig 3 full 100%

--- Assignments/3/Big_Chance_Num.cpp
+++ Assignments/3/Big_Chance_Num.cpp
@@ -1,44 +1,30 @@
 #include <iostream>
-#include <math.h>
+
 using namespace std;
 
 int main()
 {
-    // n:adad vorodi "be dalil ehtemal bozorg bodan vorodi long darnazar gereftam"
     long n;
     cin >> n;
-    /*
-    tozih shart dakhele if
-        agar deghat konim mibinim ke
-        dar serie aval hazf adAdi ke i%2=1 bod baghi mandand
-        dar serie dovom hazf adAdi ke i%4=1 ya i%4=3 bod baghi mandand
-        dar serie sevom hazf adAdi ke i%8=1 ya i%8=7 bod baghi mandand
-        ...
-        dar serie n om hazf adadi ke i%2^n=1 ya i%2^n=-1+2^n bod baghi mandand
-        => shart zir ra neveshtam
-    */
-    /*
-    tozih shart dakhele for
-        agar deghat konim mibinim ke
-        dar serie aval hazf adad 1 ke ghabl 2 hast baray hamishe baghi mimand
-            chon az een be bad 2 ta da mian hazf darim va 2 ghabl az baze hazf hast
-        dar serie dovom hazf adad 1,3 ke ghabl 4 hast baray hamishe baghi mimand
-            chon az een be bad 3 ta da mian hazf darim va adade zekr shode ghabl az baze hazf hast
-        dar serie sevom hazf adad 1,3,7 ke ghabl 8 hast baray hamishe baghi mimand
-            chon az een be bad 3 ta da mian hazf darim va adade zekr shode ghabl az baze hazf hast
-        ...
-        pas agar ada ta 2^round(Log2(n)) zende bemanad , baraye hamishe zende mimanad
-    */
-    for (int i = 1; pow(2, i) <= n; i++)
+    //az e'enja be bad n mahale noghte ba hazf kardan noghat sookhte da har dor ast
+    for (int i = 2; i < n; i++)
     {
-        if (n % long(pow(2, i)) == 1 || n % long(pow(2, i)) == long(pow(2, i)) - 1)
+        /*
+        agar har dast bekhahim n dar mian hazf konim pas a'dad j*(n+1) j az n ta inf 
+            hazf mishvad (n+1 dar e'en code i ast)
+        alabte ke ma to inf ro lazem nadarim va faghat to ghable adad vorodi moon kafee ast
+        agar mokhtrasate adad da har dore hazf mazrab iu bashd hazf migardad pas if zir ra neveshtam
+        */
+        if (n % i == 0)
         {
-            // continue: agar dar een serie hazf zende mand serie bad ra check konad
-            continue;
+            cout << "NO";
+            return 1;
         }
-        cout << "NO";
-        return 0;
+        // long(n / i) tedad adad hazf shode ast ke ba kam kardan A'n makan jadid adad ra baray dor jadid peyad mikonim
+        // lazem be zekr nist ke vaghti be e'en khat residim yani adad ma be dor bad rah yafte
+        n -= long(n / i);
     }
+    //agar i (tarif shode dar for) az n bistar shavad yani digar dar khatar hazf nist va az for biron miaiim 
     cout << "YES";
     return 1;
 }
